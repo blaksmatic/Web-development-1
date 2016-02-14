4979b5bcb2019d153a658e096249f9f01b25f802
blaksmatic <blaksmatic@gmail.com>
2016-02-14 20:11:24 +0000

    finaliza

--- source_js/script.js
+++ source_js/script.js
@@ -1,16 +1,92 @@
+/*referance: http://lions-mark.com/jquery/scrollTo/ */
+
 var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener("click", highlightThis);
+    /*
+     divs[i].addEventListener("click", highlightThis, true);
+     divs[i].addEventListener("click", highlightThis, false);*/
 }
 
 function highlightThis(event) {
     //event.stopPropagation();
-
     var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
+    this.style.backgroundColor = 'yellow';
     alert(this.className);
-    this.style.backgroundColor=backgroundColor;
+    this.style.backgroundColor = backgroundColor;
 }
+
+var $window = $(window)
+var $document = $(document)
+
+
+
+/*This function is resizing the navbar*/
+$document.ready(function () {
+    $window.scroll(function () {
+        if ($document.scrollTop() > 30) {
+            $('nav.navbar').addClass('small');
+        } else {
+            $('nav.navbar').removeClass('small');
+        }
+    });
+});
+
+/*this function is smooth scrolling*/
+$document.ready(function () {
+    $('a[href*=#]').on('click', function (event) {
+        if (this.id == "Trail" || this.id == "stor" || this.id == "charac" || this.id == "chrou")
+            event.preventDefault();
+        $('body').animate({scrollTop: $(this.hash).offset().top - 100}, 'slow');
+    });
+});
+
+/*This function is carousel moving right*/
+$document.ready(function () {
+    $('#navIconRight').click(function () {
+        $('#carousel ul').animate({marginLeft: -1280}, 1500, function () {
+            $(this).find("li:last").after($(this).find("li:first"));
+            $(this).css({marginLeft: 0});
+        })
+    })
+});
+
+/*This function is carousel moving left*/
+$document.ready(function () {
+    $('#navIconLeft').click(function () {
+        $('#carousel ul').animate({marginLeft: 1280}, 1500, function () {
+            $(this).find("li:first").before($(this).find("li:last"));
+            $(this).css({marginLeft: 0});
+        })
+    })
+});
+
+/*This function is position indicator*/
+$document.ready(function () {
+    var scrollDistance = $(window).scrollTop();
+    var trailerDistance = $("#Trailer").offset().top;
+    var storyDistance = $("#story").offset().top;
+    var characterDistance = $("#Characters").offset().top;
+    var crouselDistance = $("#carousel").offset().top+100;
+
+    $window.scroll(function () {
+        var CurrentPosition = $document.scrollTop();
+        if (CurrentPosition > trailerDistance && CurrentPosition <= storyDistance)
+            $("#Trail").addClass("isHere");
+        else
+            $("#Trail").removeClass("isHere");
+        if (CurrentPosition > storyDistance && CurrentPosition <= crouselDistance)
+            $("#stor").addClass("isHere");
+        else
+            $("#stor").removeClass("isHere");
+        if (CurrentPosition > crouselDistance && CurrentPosition <= characterDistance)
+            $("#chrou").addClass("isHere");
+        else
+            $("#chrou").removeClass("isHere");
+        if (CurrentPosition > characterDistance)
+            $("#charac").addClass("isHere");
+        else
+            $("#charac").removeClass("isHere");
+    })
+})
+;
